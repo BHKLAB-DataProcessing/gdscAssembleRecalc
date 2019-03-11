b6b1304a282f52a3c356898300e2e66a668957dd
Petr Smirnov <psmirnov2000@gmail.com>
2019-03-11 13:10:14 -0400

    first

---
+++ assembleData.R
@@ -0,0 +1,15 @@
+library(PharmacoGxPrivate)
+
+myfn <- list.files("/pfs/input/", full.names=TRUE)
+slices <- list()
+
+for(fn in myfn){
+	temp <- readRDS(fn)
+	parTable <- do.call(rbind,temp[[3]])
+	n <- cbind("auc_recomputed" = temp[[1]], "ic50_recomputed" = temp[[2]], parTable) 
+	slices[[fn]] <- n
+}
+
+res <- do.call(rbind, slices)
+
+saveRDS(res, file=paste("/pfs/out/", "gdscProfiles.rds"))
