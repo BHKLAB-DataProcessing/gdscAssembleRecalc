52816112fbcf2eaee0c88da3deff72104ada79a5
Petr Smirnov <psmirnov2000@gmail.com>
2019-09-16 12:33:17 -0400

    try another approach

--- assembleData.R
+++ assembleData.R
@@ -8,9 +8,13 @@ slices <- list()
 for(fn in myfn){
 	temp <- readRDS(fn)
 	parTable <- do.call(rbind,temp[[3]])
-	print(head(rownames(parTable)))
-	print(str(parTable))
-	n <- data.frame("AAC" = as.numeric(unlist(temp[[1]]))/100, "IC50" = as.numeric(unlist(temp[[2]])), apply(parTable, c(1,2), function(x) return(as.numeric(unlist(x))))) 
+	# print(head(rownames(parTable)))
+	# print(str(parTable))
+	n <- data.frame("AAC" = as.numeric(unlist(temp[[1]]))/100, 
+					"IC50" = as.numeric(unlist(temp[[2]])), 
+					"HS" = as.numeric(unlist(temp[[3]]$HS)),
+					"E_inf" = as.numeric(unlist(temp[[3]]$E_inf)),
+					"EC50" = as.numeric(unlist(temp[[3]]$EC50))) 
 	slices[[fn]] <- n
 }
 
