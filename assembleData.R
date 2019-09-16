8b70754756cf10ada10280cc80baa34bfa32cada
Petr Smirnov <psmirnov2000@gmail.com>
2019-09-16 12:25:19 -0400

    need logs

--- assembleData.R
+++ assembleData.R
@@ -8,6 +8,8 @@ slices <- list()
 for(fn in myfn){
 	temp <- readRDS(fn)
 	parTable <- do.call(rbind,temp[[3]])
+	print(head(rownames(parTable)))
+	print(str(parTable))
 	n <- data.frame("AAC" = as.numeric(unlist(temp[[1]]))/100, "IC50" = as.numeric(unlist(temp[[2]])), apply(parTable, c(1,2), function(x) return(as.numeric(unlist(x))))) 
 	slices[[fn]] <- n
 }
